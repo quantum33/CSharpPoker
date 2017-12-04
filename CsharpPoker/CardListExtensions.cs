513337e1911d17d1127fc6c82fd9d4789cf2d7d7
Thierry Castel <thierry.castel33@gmail.com>
2017-12-04 22:42:41 +0100

    chptr 5

---
+++ CsharpPoker/CardListExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CsharpPoker
+{
+    internal static class CardListExtensions
+    {
+        internal static int GetMinMaxDifference(this List<Card> cards)
+            => cards.Max(card => card.Value) - cards.Min(card => card.Value);
+    }
+}
